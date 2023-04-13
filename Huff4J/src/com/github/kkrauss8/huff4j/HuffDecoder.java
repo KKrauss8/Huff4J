017759ac7f990d58eb55b6d29a8ee8ba00261828
KKrauss8 <kjkrauss8@gmail.com>
2023-04-12 18:47:21 -0600

    renaming functions to maintain consistency with HuffDecoder class.

--- Huff4J/src/com/github/kkrauss8/huff4j/HuffDecoder.java
+++ Huff4J/src/com/github/kkrauss8/huff4j/HuffDecoder.java
@@ -1,7 +1,28 @@
 package com.github.kkrauss8.huff4j;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class HuffDecoder {
 
-	// TODO: Write Decoder
+	private Map<Character, Integer> frequencyMap;
+	private Map<Charcater, HuffCode> codeMap;
+
+	public HuffDecoder() {
+		frequencyMap = new HashMap<>();
+		codeMap = new HashMap<>();
+	}
+
+	private void rebuildFrequencyMap() {
+
+	}
+
+	private void rebuildHuffmanTree() {
+
+	}
+
+	public String decode(byte[] encodedData, byte[] charFrequencyData) {
+
+	}
 	
 }
